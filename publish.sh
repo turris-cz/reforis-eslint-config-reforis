9550fe4279fe3c9ff575d3f668aa482234498572
Aleksandr Gumroian <aleksandr.gumroian@nic.cz>
2023-02-14 15:26:50 +0100

    Fix access token in publish script
    As npm has a new access token format
    https://github.blog/changelog/2021-09-23-npm-has-a-new-access-token-format/

--- publish.sh
+++ publish.sh
@@ -5,8 +5,7 @@ then
     echo "\$NPM_TOKEN is not set"
     exit 1
 else
-    # Need to replace "_" with "-" as GitLab CI won't accept secret vars with "-"
-    echo "//registry.npmjs.org/:_authToken=$(echo "$NPM_TOKEN" | tr _ -)" > .npmrc
+    echo "//registry.npmjs.org/:_authToken=$(echo "$NPM_TOKEN")" > .npmrc
     echo "unsafe-perm = true" >> ~/.npmrc
     npm publish
 fi
