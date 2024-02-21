03a999f2aa5482582a928c53755e0b8507947cc6
Aleksandr Gumroian <aleksandr.gumroian@nic.cz>
2024-02-21 14:25:56 +0100

    Add support for JSX in Babel options

--- index.js
+++ index.js
@@ -33,6 +33,9 @@ module.exports = {
         babelOptions: {
             presets: ["@babel/preset-react"],
         },
+        ecmaFeatures: {
+            jsx: true,
+        },
     },
     globals: {
         _: "readonly",
